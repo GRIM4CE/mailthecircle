53e4b0ab4e2d1df5ed712c4a53297f0ebc0a568c
Jon Leibham <jleibham@gmail.com>
2017-12-20 17:19:47 -0600

    fix

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 4200;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Connected to server on localhost: ${port}`));
+app.listen(4200, () => console.log('Example app listening on port 4200!'))
