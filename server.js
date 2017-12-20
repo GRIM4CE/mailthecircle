ca82eab7c34c6fcd2be0a9d258337c3161c1a045
Jon Leibham <jleibham@gmail.com>
2017-12-20 17:23:21 -0600

    fixes for server

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-const express = require('express');
+'use strict';
 
-const app = express();
+// We’re setting up an extremely simple server here.
+const http = require('http');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// These could (should) be set as env vars.
+const port = process.env.PORT || 4200;
+const host = process.env.HOST || 'localhost';
 
-app.listen(4200, () => console.log('Example app listening on port 4200!'))
+// No matter what hits the server, we send the same thing.
+http.createServer((req, res) => {
+
+  // Tell the browser what’s coming.
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8',
+  });
+
+  // Send a simple message in HTML.
+  res.write('<h1>YO Buy YOUR CIRCLES HER BRO</h1>');
+  res.write('<p>And I’m <em>sooooo</em> COOL. #delego</p>');
+  res.end();
+}).listen(port, host);
+
+// This message prints in the console when the app starts.
+console.log(`App running at http://${host}:${port}`);
