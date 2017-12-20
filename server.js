1d778cc0256879517dd94fa3efdc185b46f7b741
Jon Leibham <jleibham@gmail.com>
2017-12-20 16:57:15 -0600

    changes

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 4200;
 
-app.use(express.static('dist'))
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Connected to server on localhost: ${port}`));
