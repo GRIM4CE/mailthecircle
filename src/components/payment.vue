95a418d698e796eff3b8b5d3a1696602dd5e51da
Jon Leibham <jleibham@gmail.com>
2017-12-27 17:28:12 -0600

    new form

--- src/components/payment.vue
+++ src/components/payment.vue
@@ -1,55 +1,54 @@
 <template lang="html">
   <section>
-    <form class="payment-container" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-      <div class="payment-container-header">
-        <h2>Mail a circle to a friend or foe.</h2>
-      </div>
-      <div class="payment-container-main">
-        <input type="hidden" name="cmd" value="_s-xclick">
-        <input type="hidden" name="hosted_button_id" value="LVMKNFD2HCPF4">
-        <input type="hidden" name="on0" value="Recipients Name">Recipients Name
-        <input type="text" name="os0" maxlength="200">
-        <input type="hidden" name="on1" value="Recipients Address">Recipients Address
-        <input type="text" name="os1" maxlength="200"></td>
-        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+      <form class="payment-container" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <div class="payment-container-header">
+          <h2>Mail a circle to a friend or foe.</h2>
+        </div>
+        <div class="payment-container-main">
+          <input type="hidden" name="cmd" value="_s-xclick">
+          <input type="hidden" name="hosted_button_id" value="U2TZ3HJLN2B9W">
+          <input type="hidden" name="on0" value="Recipients Name">Recipients Name
+          <input type="text" name="os0" maxlength="200">
+          <input type="hidden" name="on1" value="Recipients Mailing Address">Recipients Mailing Address
+          <input type="text" name="os1" maxlength="200">
+          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
       </div>
     </form>
   </section>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
 <style lang="scss" scoped>
-  @import '../assets/variables';
+@import '../assets/variables';
 
-  .payment-container {
+.payment-container {
     width: 400px;
     margin: 2rem auto;
     border: 1px solid $black;
     border-radius: 8px;
     overflow: hidden;
-  }
+}
 
-  .payment-container-header {
+.payment-container-header {
     background-color: $black;
-    padding: .5rem 1rem;
-  }
+    padding: 0.5rem 1rem;
+}
 
-  h2 {
+h2 {
     margin: 0;
     color: $white;
-  }
+}
 
-  .payment-container-main {
+.payment-container-main {
     padding: 1rem 2rem;
-  }
+}
 
-  input[type='text'] {
+input[type='text'] {
     width: 100%;
     margin-bottom: 1rem;
-  }
+}
 </style>
