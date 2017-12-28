18e51d9dad079fb82872534d01f62a346cd51c16
Jon Leibham <jleibham@gmail.com>
2017-12-28 13:26:08 -0600

    font updates

--- src/app.vue
+++ src/app.vue
@@ -15,14 +15,6 @@
     </div>
   </section>
 
-  <!-- We’ve spent a few hours scheming and what we’ve decided to commit our free time to offering you a service to send a friend or foe “The Circle” by mail for a phenomenal fee of $2.99. And because our commitment and love of the sport we’ve decided
-  to mail bonus circles to top celebs based on your feedback. Check our meter to see who's gonna get it next -->
-
-  <!-- "the circle will be printed on a 4'' x 6'' 16PT Postcard with a UV Coated Finish
-the price includes double sided. So we can do the congratulations note on the backside -->
-
-  <!-- "congratulations! You have just been mailed the Circle. Now get ready to receive two hits from one of your friends. We can't tell you who sent it so don't forget to always be looking over your shoulder and get ready for hell on earth" -->
-
   <section class="section about">
     <div class="section-wrapper">
       <h1 class="section-header">The Why</h1>
@@ -53,15 +45,15 @@ the price includes double sided. So we can do the congratulations note on the ba
     </div>
   </section>
 
+  <!-- "congratulations! You have just been mailed the Circle. Now get ready to receive two hits from one of your friends. We can't tell you who sent it so don't forget to always be looking over your shoulder and get ready for hell on earth" -->
+
   <section class="section payment">
     <div class="section-wrapper">
       <h1 class="section-header">Are you ready to Mail The Circle?</h1>
       <h2 class="section-subheader">We thought so.</h2>
       <div class="col-2">
-        <p class="payment-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt minima accusamus, aperiam esse iusto unde corrupti animi beatae consequatur, vitae, soluta molestiae harum illo ad quidem enim. Temporibus optio accusantium dolorem. Consequuntur
-          esse inventore repudiandae tenetur quod perspiciatis provident deserunt explicabo incidunt sed aspernatur possimus voluptatem optio rem nam, adipisci, ducimus, deleniti. Omnis provident sunt laborum adipisci enim consequatur, in ipsum eum maxime,
-          d
-        </p>
+        <p class="payment-text">Alright so you've decided to Mail The Circle, we know you wont regret it. Just complete the paypal payment form with the recepiants name and address and leave the rest to us.
+        The circle will be printed on a 4" x 6" 16PT Postcard with a UV Coated Finish so you know it's something that will hang around. </p>
       </div>
       <div class="col-2" id="payments">
         <payment></payment>
